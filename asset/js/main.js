0f4f610c15b3b4e0fdc7276de0b56e8131b94d4a
StefanoNesi <sn.boolean@gmail.com>
2021-10-13 14:04:39 +0200

    Put down idea on paper

---
+++ asset/js/main.js
@@ -0,0 +1,14 @@
+/*
+
+Step 1 - Create a cycle that writes numbers from 1 to 100
+Step 2 - Append those items to a container
+Step 3 - Give some items (determined by a condition) a different style
+
+#Tools
+    - For
+    - append / insertAdjacentHTML
+    - Ifs (for style control)
+    - CSS for page layout
+
+
+*/
